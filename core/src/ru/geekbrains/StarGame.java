38d0e906fe01a397489698da40f72f269c181dda
listpv <listpv@mail.ru>
2020-10-15 14:18:23 +0300

    Выполнено ДЗ№1.

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float point;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		point = 0f;
 	}
 
 	@Override
@@ -21,8 +23,11 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 650, 480);
+		batch.draw(img, 200, 120);
+		batch.draw(img, point, point, 100, 100);
 		batch.end();
+		point += 1;
 	}
 	
 	@Override
