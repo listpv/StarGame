af70139a4bd919ef68d08ce292bd3fd8639fde0c
listpv <listpv@mail.ru>
2020-10-21 14:57:43 +0300

    ДЗ №2 обновление после урока №3.

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -9,22 +9,26 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    public static final float V_LEN = 0.5f;
+
     private Texture img;
     private Vector2 pos;
     private Vector2 destPos;  //  точка клика мышью.
     private Vector2 v;
     private Vector2 tempV;    //  вспомогательная переменная для обработки keyDown.
-    private float distance;   //  расстояние между клика мышью и картинкой.
+//    private float distance;   //  расстояние между клика мышью и картинкой.               1-ый способ
+    private Vector2 tempPos;    // вектор для временного хранения destPos, дабы не делать cpy() в render.   2-ой способ
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        pos = new Vector2(100, 100);
-        destPos = pos.cpy();
-        v = new Vector2(0, 0);
+        pos = new Vector2();
+        destPos = new Vector2();
+        v = new Vector2();
+        tempPos = new Vector2();
         tempV = v.cpy();
-        distance = 0;
+//        distance = 0;            // 1-ый способ.
     }
 
     @Override
@@ -33,20 +37,33 @@ public class MenuScreen extends BaseScreen {
         batch.begin();
         batch.draw(img, pos.x, pos.y);
         batch.end();
-        if(distance <= v.len())
+
+//      1-ый способ. мой
+//        if(distance <= v.len())
+//        {
+//            v.setZero();
+//        }
+//        distance -= v.len();
+//        pos.add(v);
+
+//      2-ой способ.
+        tempPos.set(destPos);
+        if(tempPos.sub(pos).len() <= v.len())
         {
-            v.setZero();
-        }
-        distance -= v.len();
-        pos.add(v);
+            pos.set(destPos);
+        }else
+            {
+                pos.add(v);
+            }
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         destPos.set(screenX, Gdx.graphics.getHeight() - screenY);
         v = destPos.cpy().sub(pos);
-        distance = v.len();
-        v.scl(0.01f);
+//        distance = v.len();      //  1-ый способ.
+        v.setLength(V_LEN);        //  скорость не меняется.
+//        v.scl(0.01f);           //  скорость меняется.
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
